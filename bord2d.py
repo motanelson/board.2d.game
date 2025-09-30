549ddcb5cb68376db51b11e4943048ec57531662
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-30 08:58:49 +0100

    Add files via upload

--- bord2d.py
+++ bord2d.py
@@ -1,4 +1,28 @@
 print("\033c\033[43;30m\nboard\n")
+def draw(arrays):
+    print("\033c\033[43;30m\n")
+    a="0123456789ABCDEF"
+    counts=0
+    counts2=0
+    print(" "+a)
+    for nn in range(len(arrays)):
+        print(a[counts2],end="")
+        counts=0
+        
+        for n in range(len(arrays[nn])):
+            print(arrays[nn][n],end="")
+            counts=counts+1
+        print((len(a)-counts)*" "+a[counts2])
+        counts2=counts2+1
+    print(" "+a)
+
+def board():
+    print("\033c\033[43;30m\n")
+    a="0123456789ABCDEF"
+    print(" "+a)
+    for n in a:
+        print(n+(" "*16)+n)
+    print(" "+a)
 def saves(files,arrays):
     f1=open(files,"w")
     for n in arrays:
@@ -22,6 +46,5 @@ def board2d(x,y):
     return a
 
 
-a=board2d(2,2)
-saves("2d.csv",a)
-print(a)
+a=board2d(16,16)
+draw(a)
